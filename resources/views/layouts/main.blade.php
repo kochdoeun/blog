927600b7956628f37f7a5f801098dddd15e1b50d
Doeun KOCH <doeunkoch@gmail.com>
2016-10-26 22:09:53 +0700

    files

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta 
-			http-equiv="Content-Type" 
+		<meta
+			http-equiv="Content-Type"
 			content="text/html; charset=UTF-8"
 		/>
-		<meta 
-			name="viewport" 
+		<meta
+			name="viewport"
 			content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
 		/>
 		<title> @yield('title')</title>
-		<link 
-			href="https://fonts.googleapis.com/icon?family=Material+Icons" 
+		<link
+			href="https://fonts.googleapis.com/icon?family=Material+Icons"
 			rel="stylesheet">
-		<link 
-			href="{{ asset('css/materialize.css') }}" 
-			rel="stylesheet" 
-			type="text/css" 
+		<link
+			href="{{ asset('css/materialize.css') }}"
+			rel="stylesheet"
+			type="text/css"
 		/>
-		<link 
-			href="{{ asset('css/app.css') }}" 
-			rel="stylesheet" 
-			type="text/css" 
+		<link
+			href="{{ asset('css/app.css') }}"
+			rel="stylesheet"
+			type="text/css"
 		/>
 		@yield('include_css')
 	</head>
@@ -35,17 +35,21 @@
 		<div class="footer">
 			@yield('footer')
 		</div>
-		<script 
-			type="text/javascript" 
+		<script
+			type="text/javascript"
 			src="{{ asset('js/jquery-2.1.1.min.js') }}"
 		></script>
-		<script 
-			type="text/javascript" 
+		<script
+			type="text/javascript"
 			src="{{ asset('js/materialize.js') }}"
 		></script>
-		<script 
-			type="text/javascript" 
+		<script
+			type="text/javascript"
 			src="{{ asset('js/init.js') }}"
+		></script>
+		<script
+				type="text/javascript"
+				src="{{ asset('js/jquery.validate.min.js') }}"
 		></script>
 		@yield('include_footer_js')
 	</body>
--- resources/views/partials/auth/register/step1.blade.php
+++ resources/views/partials/auth/register/step1.blade.php
@@ -1,80 +1,84 @@
 <div id="step1">
-    <form class="col s12">
-        <h5>1. Basic information</h5>
-        <div class="row">
-        	<div class="input-field col s12">
-        	  <input placeholder="Your listing name" id="listingName" type="text" class="validate">
-        	  <label for="listingName">Listing Name</label>
-        	</div>
-        </div>
+	<div
+		class="col s12"
+		id=""
+	>
+		<h5>1. Basic information</h5>
+		<div class="row">
+			<div class="input-field col s12">
+				<input placeholder="Your listing name" id="listingName" type="text" class="validate">
+				<label for="listingName">Listing Name</label>
+			</div>
+		</div>
 
-        <div class="row">
-        	<div class="file-field input-field col s12">
-        		<div class="btn">
-        			<span>File</span>
-        			<input type="file" id="logo">
-        		</div>
-        		<div class="file-path-wrapper">
-        			<input class="file-path validate" type="text" placeholder="Upload your logo">
-        		</div>
-        	</div>
-        </div>
+		<div class="row">
+			<div class="file-field input-field col s12">
+				<div class="btn">
+					<span>File</span>
+					<input type="file" id="logo">
+				</div>
+				<div class="file-path-wrapper">
+					<input class="file-path validate" type="text" placeholder="Upload your logo">
+				</div>
+			</div>
+		</div>
 
-        <div class="row">
-        	<div class="input-field col s12">
-        	  <input placeholder="write down about ur business" id="Description" type="text" class="validate">
-        	  <label for="Description">Description</label>
-        	</div>
-        </div>
+		<div class="row">
+			<div class="input-field col s12">
+				<input placeholder="write down about ur business" id="Description" type="text" class="validate">
+				<label for="Description">Description</label>
+			</div>
+		</div>
 
-        <div class="row">
-        	<div class="input-field col s12">
-        	  <input placeholder="Listing category" id="category" type="text" class="validate">
-        	  <label for="category">Listing category</label>
-        	</div>
-        </div>
+		<div class="row">
+			<div class="input-field col s12">
+				<input placeholder="Listing category" id="category" type="text" class="validate">
+				<label for="category">Listing category</label>
+			</div>
+		</div>
 
-        <div class="row">
-        	<div class="input-field col s12">
-        	  <input placeholder="Visitors can filter their search by the amenities too, so make sure you include all the relevant ones." id="tag" type="text" class="validate">
-        	  <label for="tag">Listing tags (optional)</label>
-        	</div>
-        </div>
+		<div class="row">
+			<div class="input-field col s12">
+				<input placeholder="Visitors can filter their search by the amenities too, so make sure you include all the relevant ones."
+					   id="tag" type="text" class="validate">
+				<label for="tag">Listing tags (optional)</label>
+			</div>
+		</div>
 
-        <h5>2. Contact Information</h5>
+		<h5>2. Contact Information</h5>
 
-        <div class="row">
-        	<div class="input-field col s12">
-        	  <input placeholder="e.g +855 70-313-638" id="bphone" type="text" class="validate">
-        	  <label for="bphone">Phone (optional)</label>
-        	</div>
-        </div>
+		<div class="row">
+			<div class="input-field col s12">
+				<input placeholder="e.g +855 70-313-638" id="bphone" type="text" class="validate">
+				<label for="bphone">Phone (optional)</label>
+			</div>
+		</div>
 
-        <div class="row">
-        	<div class="input-field col s12">
-        	  <input placeholder="e.g aseapages.com, Phnom Penh" id="website" type="text" class="validate">
-        	  <label for="website">Website (optional)</label>
-        	</div>
-        </div>
+		<div class="row">
+			<div class="input-field col s12">
+				<input placeholder="e.g aseapages.com, Phnom Penh" id="website" type="text" class="validate">
+				<label for="website">Website (optional)</label>
+			</div>
+		</div>
 
-        <div class="row">
-        	<div class="input-field col s12">
-        	  <input placeholder="e.g @youwebsite" id="email" type="text" class="validate">
-        	  <label for="email">E-mail Address</label>
-        	</div>
-        </div>
+		<div class="row">
+			<div class="input-field col s12">
+				<input placeholder="e.g @youwebsite" id="email" type="text" class="validate">
+				<label for="email">E-mail Address</label>
+			</div>
+		</div>
 
-        <h5>3. Listing on Aseapages</h5>
+		<h5>3. Listing on Aseapages</h5>
 
-        <div class="row">
-        	<div class="input-field col s12">
-        	  <input placeholder="e.g coffee shop, hotel, guesthouse" id="Keyword" type="text" class="validate">
-        	  <label for="Keyword">Keyword</label>
-        	</div>
-        </div>
+		<div class="row">
+			<div class="input-field col s12">
+				<input placeholder="e.g coffee shop, hotel, guesthouse" id="Keyword" type="text" class="validate">
+				<label for="Keyword">Keyword</label>
+			</div>
+		</div>
 
-        <a class="waves-effect waves-light btn">
-            <i class="material-icons right">cloud</i>Next
-        </a>
-    </form>
+		<a class="waves-effect waves-light btn">
+			<i class="material-icons right">cloud</i>Next
+		</a>
+	</div>
 </div>
