68c67c087117881289464cf0d403c14040543b1b
Pak Udom <pak.udomkh@gmail.com>
2016-10-29 00:50:42 +0700

    udom is very cute

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -6,81 +6,431 @@
 
 @section('include_content')
 
-<img src="img/b-banner2.png" alt="Unsplashed background img 1" style="width: 100%; margin-bottom: 10px;">
 
-<div class="container">
-	<div class="row">
-		<div class="col s3">
-			<a href="">About GoOnPages</a><br/>
-			<a href="">Our Team</a><br/>
-			<a href="">Our Vision</a><br/>
-			<a href="">Our Mission</a><br/><br/>
-		
-			<a href="">Why we are?</a><br/>
-			<a href="">For SMe</a><br/>
-			<a href="">For Business</a><br/>
-			<a href="">For People</a><br/><br/>
+<div class="section" style="background:url('img/bg-header.jpg'); min-height:600px;">
+
+	<nav class="udt-about-nav-container" style="background:transparent; border:none; box-shadow: none;  position: fixed; z-index: 1;">
+		<div class="container">
+			<div class="nav-wrapper">
+			  <ul id="nav-mob#" class="udt-about-nav right hide-on-med-and-down">
+				<li><a href="#">ABOUT</a></li>
+				<li><a href="#">SERVICES</a></li>
+				<li><a href="#">BLOG</a></li>
+				<li><a href="#">PRICING</a></li>
+				<li><a href="#">CONTACT</a></li>
+			  </ul>
+			</div>
+		</div>
+	</nav>
+	
+	
+	<br/><br/><br/><br/><br/><br/><br/>
+	<h1 class="center white-text">UDT DEVELOPER</h1>
+	<h5 class="center white-text">Since SMEs concerning of its products to be published reaching global market as well as finding business partners</h5>
+	
+</div>
+
+
+<div class="section">
+
+	<div class="container">
+	
+		<!-- BOUT US ------------- -->
+		<h4><b>WHO WE ARE</b></h4>
+		<div class="row">
+			<div class="col m7 s12">
+			Since SMEs concerning of its products to be published reaching global market as well as finding business partners. AseaPages has cleared vision in offering online advertisement services which can help companies to gain more business exposure and bring them to the global scale.
+			<br/><br/>
+
+Transform AseaPages to become one productions base exhibition and searching resources for every company within ASIA and generate customers from around the world through AseaPages platform, especially AseaPages will be able to make the world know clearly about business performance in ASIA.
+<br/><br/>Regarding to quality reason, our teams mission is to collects either trustable enterprises or industries in ASIA then categorize it into specific category and location so that visitors can easily notify your properties to access your information on our platform.
+			</div>
+			
+			<div class="col m5 s12">
+				<img src="img/1.jpg" style="width:100%;"/>
+			</div>
 			
 		</div>
 		
+		<h4><b>MEET OUR TEAM</b></h4>
+		<div class="row">
+			<div class="col m4 s12">
+				  <div class="card">
+						<div class="card-image">
+						  <img src="/img/udom.jpg">
+						  <span class="card-title">PAK UDOM</span>
+						</div>
+						<div class="card-content">
+						  <p>I am a very simple card. I am good at containing small bits of information.
+						  I am convenient because I require little markup to use effectively.</p>
+						</div>
+					</div>
+			</div>
+			
+			<div class="col m4 s12">
+				  <div class="card">
+						<div class="card-image">
+						  <img src="/img/doeun.jpg">
+						  <span class="card-title">KOCH DOEUN</span>
+						</div>
+						<div class="card-content">
+						  <p>I am a very simple card. I am good at containing small bits of information.
+						  I am convenient because I require little markup to use effectively.</p>
+						</div>
+					</div>
+			</div>
+			
+			<div class="col m4 s12">
+				  <div class="card">
+						<div class="card-image">
+						  <img src="/img/trik.jpg">
+						  <span class="card-title">LOAT TRE</span>
+						</div>
+						<div class="card-content">
+						  <p>I am a very simple card. I am good at containing small bits of information.
+						  I am convenient because I require little markup to use effectively.</p>
+						</div>
+					</div>
+			</div>
+			
 		
-		<div class="col s9">
-			
-			People Talking About Us
-			We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated. We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.
-
-			Company Bio
-			We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.
-
-			Site map
-			Contact Us
-			About Us
-			Our Vision
-			Our Mission
-			Connect
-			Facebook
-			Yourtube
-			Link-in
-			Twitter <br/><br/>
-			
-			People Talking About Us
-			We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated. We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.
-
-			Company Bio
-			We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.
-
-			Site map
-			Contact Us
-			About Us
-			Our Vision
-			Our Mission
-			Connect
-			Facebook
-			Yourtube
-			Link-in
-			Twitter <br/><br/>
-			
-			People Talking About Us
-			We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated. We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.
-
-			Company Bio
-			We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.
-
-			Site map
-			Contact Us
-			About Us
-			Our Vision
-			Our Mission
-			Connect
-			Facebook
-			Yourtube
-			Link-in
-			Twitter <br/><br/>
 		</div>
+		<!-- /BOUT US ------------- -->
+		
+		
+		
+		
+		
+		<!-- OUR SERVICES ------------- -->
+		<h4><b>WHAT WE DO</b></h4>
+		
+		Since SMEs concerning of its products to be published reaching global market as well as finding business partners. AseaPages has cleared vision in offering online advertisement services which can help companies to gain more business exposure and bring them to the global scale.
+			<br/><br/>
+
+Transform AseaPages to become one productions base exhibition and searching resources for every company within ASIA and generate customers from around the world through AseaPages platform, especially AseaPages will be able to make the world know clearly about business performance in ASIA.
+<br/><br/>Regarding to quality reason, our teams mission is to collects either trustable enterprises or industries in ASIA then categorize it into specific category and location so that visitors can easily notify your properties to access your information on our platform.
+
+
+		<h4><b>SOLUTION</b></h4>
+		<p>Make sure you are found when someone around the world is looking for your products</p>
+		
+		<div class="row">
+			<div class="col m3 s6">
+				<div class="card">
+						<div class="card-image light-blue darken-4">
+							<i class="fa fa-search fa-5x" aria-hidden="true"></i>
+							<span class="card-title">GET FOUND</span>
+						</div>
+						<div class="card-content">
+						  <p>I am a very simple card. I am good at containing small bits of information.
+						  I am convenient because I require little markup to use effectively.</p>
+						</div>
+				</div>
+			</div>
+			
+			<div class="col m3 s6">
+				<div class="card">
+						<div class="card-image red">
+							<i class="fa fa-book fa-5x" aria-hidden="true"></i>
+							<span class="card-title">SHOWCASE</span>
+						</div>
+						<div class="card-content">
+						  <p>I am a very simple card. I am good at containing small bits of information.
+						  I am convenient because I require little markup to use effectively.</p>
+						</div>
+				</div>
+			</div>
+			
+			<div class="col m3 s6">
+				<div class="card">
+						<div class="card-image teal accent-4">
+							<i class="fa fa-list-alt fa-5x" aria-hidden="true"></i>
+							<span class="card-title">ADVERTISE</span>
+						</div>
+						<div class="card-content">
+						  <p>I am a very simple card. I am good at containing small bits of information.
+						  I am convenient because I require little markup to use effectively.</p>
+						</div>
+				</div>
+			</div>
+			
+			<div class="col m3 s6">
+				<div class="card">
+						<div class="card-image teal accent-4">
+							<i class="fa fa-book fa-5x" aria-hidden="true"></i>
+							<span class="card-title">REACH</span>
+						</div>
+						<div class="card-content">
+						  <p>I am a very simple card. I am good at containing small bits of information.
+						  I am convenient because I require little markup to use effectively.</p>
+						</div>
+				</div>
+			</div>	
+		</div>
+		
+		
+		
+		
+		<h4><b>POWERFULL TOOLS</b></h4>
+		<div class="row">
+			<div class="col m3 s6">
+				<div class="card">
+						<div class="card-image light-blue darken-4">
+							<i class="fa fa-search fa-5x" aria-hidden="true"></i>
+							<span class="card-title">Business Registration</span>
+						</div>
+						<div class="card-content">
+						  <p>I am a very simple card. I am good at containing small bits of information.
+						  I am convenient because I require little markup to use effectively.</p>
+						</div>
+				</div>
+			</div>
+			
+			<div class="col m3 s6">
+				<div class="card">
+						<div class="card-image red">
+							<i class="fa fa-book fa-5x" aria-hidden="true"></i>
+							<span class="card-title">Write for the listing</span>
+						</div>
+						<div class="card-content">
+						  <p>I am a very simple card. I am good at containing small bits of information.
+						  I am convenient because I require little markup to use effectively.</p>
+						</div>
+				</div>
+			</div>
+			
+			<div class="col m3 s6">
+				<div class="card">
+						<div class="card-image teal accent-4">
+							<i class="fa fa-list-alt fa-5x" aria-hidden="true"></i>
+							<span class="card-title">Optimize</span>
+						</div>
+						<div class="card-content">
+						  <p>I am a very simple card. I am good at containing small bits of information.
+						  I am convenient because I require little markup to use effectively.</p>
+						</div>
+				</div>
+			</div>
+			
+			<div class="col m3 s6">
+				<div class="card">
+						<div class="card-image teal accent-4">
+							<i class="fa fa-book fa-5x" aria-hidden="true"></i>
+							<span class="card-title">Appearing Where People Click</span>
+						</div>
+						<div class="card-content">
+						  <p>I am a very simple card. I am good at containing small bits of information.
+						  I am convenient because I require little markup to use effectively.</p>
+						</div>
+				</div>
+			</div>	
+		</div>
+		
+		
+		<!-- /OUR SERVICES ------------- -->
+		
+	
+
+		<!-- BLOG ------------- -->
+		<h4><b>LATEST BLOG</b></h4>
+		<div class="row">
+			<div class="col m4 s12">
+				<img class="responsive-img" src="/img/baggage-rfid.jpg"/>
+				<h6><b>Aseapages Business Directory Co,LTD.</b></h6>
+				<p class="text-line-height">We are the trade association for the world's airlines. Our 265 members comprise 83% of total air traffic.</p>
+				<p>PRESS RELEASE19.10.2016</p>
+			</div>
+			
+			<div class="col m4 s12">
+				<img class="responsive-img" src="/img/airport-experience.jpg"/>
+				<h6><b>Cambodia Air Traffic Services Co,LTD</b></h6>
+				<p class="text-line-height">We are the trade association for the world's airlines. Our 265 members comprise 83% of total air traffic.</p>
+				<p>PRESS RELEASE19.10.2016</p>
+			</div>
+			
+			<div class="col m4 s12">
+				<img class="responsive-img" src="/img/passengers.jpg"/>
+				<h6><b>Cambodia Airport Nation.</b></h6>
+				<p class="text-line-height">We are the trade association for the world's airlines. Our 265 members comprise 83% of total air traffic.</p>
+				<p>PRESS RELEASE19.10.2016</p>
+			</div>
+			
+			<a class="waves-effect blue-grey lighten-1 btn right" style="margin:20px 10px 0 0">VIEW MORE</a>
+		</div>
+		
+		<!-- /BLOG ------------- -->
 		
 	</div>
-
 </div>
+
+
+<div class="section blue-grey center" style="padding:50px 0;">
+	<h5 class="white-text">ARE YOU READY FOR LISTING YOUR BUSINESS ON GOONPAGES?</h5>
+	<a class="waves-effect blue-grey lighten-1 btn" style="margin:20px 10px 0 0">CONTACT US</a> or 
+	<a class="waves-effect blue-grey lighten-1 btn" style="margin:20px 10px 0 10px">REGISTER NOW</a>
+</div>
+
+
+
+<div class="section">
+	<div class="container">
 	
+	
+		<!-- PRICING ------------- -->
+		<h4><b>GOONPAGES PRICING</b></h4>
+		<div class="row">
+			<div class="col m4 s12">
+				<div class="card">
+						<div class="card-image light-blue darken-4 center white-text" style="padding:15px 0;">
+							<h6><b>BASIC</b></h6>
+							<H4>FREE</H4>
+							<h6>UNLIMITED DATE</h6>
+						</div>
+						<div class="card-content">
+							- Free Access<br/>
+							- 2 Unique Concepts<br/>
+							- 5 Revisions<br/>
+							- Template design<br/>
+							- Secure data<br/>
+							- 24/7 Support<br/>
+							- 2 Unique Concepts<br/>
+							- 5 Revisions<br/>
+							- Template design<br/>
+							- Secure data<br/>
+							- 24/7 Support<br/>
+						</div>
+						<div class="card-action center">
+						  <a class="waves-effect waves-light btn red darken-4">GET PLAN</a>
+						</div>
+				</div>
+			</div>
+			
+			<div class="col m4 s12">
+				<div class="card">
+						<div class="card-image red darken-4 center white-text" style="padding:15px 0;">
+							<h6><b>STANDARD</b></h6>
+							<H4>$68</H4>
+							<h6>PER YEAR</h6>
+						</div>
+						<div class="card-content">
+							- Free Access<br/>
+							- 2 Unique Concepts<br/>
+							- 5 Revisions<br/>
+							- Template design<br/>
+							- Secure data<br/>
+							- 24/7 Support<br/>
+							- 2 Unique Concepts<br/>
+							- 5 Revisions<br/>
+							- Template design<br/>
+							- Secure data<br/>
+							- 24/7 Support<br/>
+						</div>
+						<div class="card-action center">
+						  <a class="waves-effect waves-light btn red darken-4">GET PLAN</a>
+						</div>
+				</div>
+			</div>
+			
+			<div class="col m4 s12">
+				<div class="card">
+						<div class="card-image light-blue darken-4 center white-text" style="padding:15px 0;">
+							<h6><b>PREMIUM</b></h6>
+							<H4>$98</H4>
+							<h6>PER YEAR</h6>
+						</div>
+						<div class="card-content">
+							- Free Access<br/>
+							- 2 Unique Concepts<br/>
+							- 5 Revisions<br/>
+							- Template design<br/>
+							- Secure data<br/>
+							- 24/7 Support<br/>
+							- 2 Unique Concepts<br/>
+							- 5 Revisions<br/>
+							- Template design<br/>
+							- Secure data<br/>
+							- 24/7 Support<br/>
+						</div>
+						<div class="card-action center">
+						  <a class="waves-effect waves-light btn red darken-4">GET PLAN</a>
+						</div>
+				</div>
+			</div>
+		</div>
+		
+		
+		<!-- PRICING ------------- -->
+		
+		
+		
+		
+		
+		<!-- CONTACT ------------- -->
+		<h4><b>GET IN TOUCH</b></h4>
+		
+		<form class="col s12">
+		  <div class="row">
+		  
+			<div class="input-field col s12">
+			  <input id="Name" type="text" class="validate">
+			  <label for="Name" data-error="wrong" data-success="right">Name</label>
+			</div>
+		  
+			<div class="input-field col s12">
+			  <input id="email" type="email" class="validate">
+			  <label for="email" data-error="wrong" data-success="right">Email</label>
+			</div>
+		  
+			<div class="input-field col s12">
+			  <textarea id="textarea1" class="materialize-textarea"></textarea>
+			  <label for="textarea1">Messages</label>
+			</div>
+			
+			<div class="input-field col s12 right">
+				<a class="waves-effect waves-light btn red darken-4">SEND MESSAGE</a>
+			</div>
+			
+		  </div>
+		</form>
+		
+		<!-- /CONTACT ------------- -->
+		
+		
+	</div>
+</div>
+
+
+
+<br/><br/><br/><br/><br/><br/><br/>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script>
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if(st>=100){
+	   $(".udt-about-nav-container").css("background","#607d8b");
+	   $(".udt-about-nav-container").css("top","63px");
+	   $(".udt-about-nav li a").css("color","#1d1c1c !important");
+   }
+   else{
+	   $(".udt-about-nav-container").css("background","transparent");
+	   $(".udt-about-nav-container").css("top","75px");
+	   $(".udt-about-nav li a").css("color","white !important");
+   }
+   /*
+   if (st > lastScrollTop){
+       // downscroll code
+   } else {
+      // upscroll code
+   }
+   lastScrollTop = st;
+   */
+});
+</script>
+
+
 
 @endsection
